f8a555bb0cb96b6491919453724af80416dbf473
vtu29441 <vtu29441@veltech.edu.in>
2026-09-10 12:00:20 +0530

    ✅ Palindrome Index | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-10

---
+++ hackerrank/easy/palindrome-index/solution.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Solution {
+
+    static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    static int palindromeIndex(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+
+                if (isPalindrome(s, left + 1, right)) {
+                    return left;
+                }
+
+                if (isPalindrome(s, left, right - 1)) {
+                    return right;
+                }
+
+                return -1;
+            }
+
+            left++;
+            right--;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int q = sc.nextInt();
+
+        while (q-- > 0) {
+            String s = sc.next();
+            System.out.println(palindromeIndex(s));
+        }
+
+        sc.close();
+    }
+}
