16a54db5a356c73c221eeb0e949b96116b56a0a4
vtu29441 <vtu29441@veltech.edu.in>
2026-09-10 11:51:39 +0530

    ✅ String Similarity | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-10

--- hackerrank/medium/string-similarity/solution.java
+++ hackerrank/medium/string-similarity/solution.java
@@ -8,8 +8,11 @@ public class Solution {
         int[] z = new int[n];
 
         z[0] = n;
-        int left = 0, right = 0;
-        long answer = n;
+
+        int left = 0;
+        int right = 0;
+
+        long sum = n;
 
         for (int i = 1; i < n; i++) {
 
@@ -27,10 +30,10 @@ public class Solution {
                 right = i + z[i] - 1;
             }
 
-            answer += z[i];
+            sum += z[i];
         }
 
-        return answer;
+        return sum;
     }
 
     public static void main(String[] args) {
